ea285dc17f3c0595a1a656fcf8417587c1ae96bb
gtbnhyujmj <gtbnhyujmj@gmail.com>
2025-06-01 21:52:07 +0800

    Update and rename endpoints.py to serverURL.py

--- serverURL.py
+++ serverURL.py
@@ -1,6 +1,10 @@
+# 放伺服器位址。
 # 集中管理所有 API 路徑（V1/V2/V3）。
+
+# 根網址
+API_URL = "https://open-api.bingx.com"
+API_URL_VST = ""
 
 # ... 依需求加入其他API路徑
 SWAP_POSITION_DUAL = '/openApi/swap/v1/positionSide/dual'
 USER_COMMISSION_RATE = '/openApi/swap/v2/user/commissionRate'
-
